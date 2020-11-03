a9cc14096848b3c791ea2b62eb505688b81d96fe
Sheep-realms <38580457+sheep-realms@users.noreply.github.com>
2020-11-03 21:30:17 +0800

    Add files via upload

--- converter/js/common.js
+++ converter/js/common.js
@@ -2,6 +2,16 @@ $('#infobox-close').click(function() {
     $('#infobox').removeClass("show");
 })
 
+function getUrlPmt(variable){
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
+
 function out(text, type) {
     $('#infobox #infobox-content').text(text);
     $('#infobox').removeClass("err");
