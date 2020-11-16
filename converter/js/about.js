e08ff8df1f960c1e45338f22f14130b879bd4ccf
Sheep-realms <38580457+sheep-realms@users.noreply.github.com>
2020-11-16 21:28:21 +0800

    Add files via upload

---
+++ converter/js/about.js
@@ -0,0 +1,23 @@
+var project = {
+    "name":         "SPR Converter",
+    "author":       "SHEEP REALMS",
+    "version":      "20201116a",
+};
+
+function about() {
+    $('#about-info').text('');
+
+    function print(item,value) {
+        $('#about-info').append('<dt>'+item+'</dt><dd><p>'+value+'</p></dd>');
+    }
+
+    print("项目名称",project.name);
+    print("作者",project.author);
+    print("版本",project.version);
+    print("","<hr>");
+
+    print("","<b>环境信息</b>");
+    print("User Agent",navigator.userAgent);
+    print("日期与时间",Date());
+    print("时间戳",Date.now());
+}
