1c3430018968496b6600d6c9867cb433e215ee89
Sheep-realms <38580457+sheep-realms@users.noreply.github.com>
2020-11-21 15:39:44 +0800

    Add files via upload

--- converter/js/editor_bbcode.js
+++ converter/js/editor_bbcode.js
@@ -59,6 +59,8 @@ $('button[class*="btn"]').click(function() {
         case 'i':
         case 'u':
         case 's':
+        case 'quote':
+        case 'code':
             jsontext = BBCode(code, seltxt);
             break;
         case 'hr':
@@ -70,36 +72,113 @@ $('button[class*="btn"]').click(function() {
         case 'size':
             showIpt('iptbox-size');
             break;
-        case 'url':
-            showConIpt('url', '显示文本', '链接地址', null, null, seltxt, "http://");
+        case 'p':
+            showConIpt('p', '内容', '行高', '缩进字符', '对齐方式', '{{{SELECTTEXT}}}', "30", "2", "left", "需要自动排版的内容（使用“{{{SELECTTEXT}}}”指代选中的内容）", "每行的高度（单位：像素）", "首行缩进的字符数", "left / centent / right", true);
             return;
+        case 'align':
+            jsontext = BBCode(code, seltxt, $(this).data('v1').toLowerCase());
+            break;
+        case 'url':
+            showConIpt('url', '显示文本', '链接地址', null, null, seltxt, "http://", "", "", "显示在链接上的内容", "链接地址");
+            return;
+        case 'img':
+            showConIpt('url', '图片地址', '宽度', '高度', null, seltxt, "", "", "", "图片文件的链接地址", "图片的宽度（填写此参数必须填写高度）", "图片的高度");
+            return;
+        default:
+            var v0t = $(this).data('v0-title');
+            var v1t = $(this).data('v1-title');
+            var v2t = $(this).data('v2-title');
+            var v3t = $(this).data('v3-title');
+            var v0tp = $(this).data('v0-tip');
+            var v1tp = $(this).data('v1-tip');
+            var v2tp = $(this).data('v2-tip');
+            var v3tp = $(this).data('v3-tip');
+            showConIpt(
+                code,
+                v0t, v1t, v2t, v3t,
+                seltxt, "", "", "",
+                v0tp, v1tp, v2tp, v3tp
+            );
     }
 
     $('#'+$(this).parent().parents('.editor').data('obj')).focus();
     $('#'+$(this).parent().parents('.editor').data('obj')).insertAtCaret(jsontext.text, jsontext.selStart, jsontext.selEnd);
 })
 
+$('button[class*="btn"]').dblclick(function() {
+    var jsontext = {};
+    var textid = $(this).parent().parents('.editor').data('obj');
+    var seltxt = selText(textid);
+    var code = $(this).data('code').toLowerCase();
+
+    switch (code) {
+        case 'p':
+            iptbarOut();
+            return;
+        case 'url':
+        case 'img':
+            jsontext = BBCode(code, seltxt);
+            break;
+        default:
+            return;
+    }
+
+    showIpt();
+    $('#'+$(this).parent().parents('.editor').data('obj')).focus();
+    $('#'+$(this).parent().parents('.editor').data('obj')).insertAtCaret(jsontext.text, jsontext.selStart, jsontext.selEnd);
+})
+
 function showIpt(id=null) {
+    if (id!=null) {
+        if ($('#'+id).hasClass('close') != true && id != "iptbox") {
+            $('#'+id).addClass('close');
+            return;
+        }
+    }
     $('.ipt-bar').addClass('close');
+
     if(id!=null) $('#'+id).removeClass('close');
 }
 
-function showConIpt(code, vt0, vt1=null, vt2=null, vt3=null, v0="", v1="", v2="", v3="") {
+function showConIpt(code, vt0, vt1=null, vt2=null, vt3=null, v0="", v1="", v2="", v3="", vm0="", vm1="", vm2="", vm3="", vspt=false) {
     $('#iptbox').attr('data-code',code);
+    $('#iptbox').attr('data-vspt',vspt);
+    //if (vspt==true) $('#iptbox').attr('data-vspt',"1");
     $('#iptbox .vline').addClass('hide');
     $('#iptbox .v').val('');
     $('#iptbox .vl0').removeClass('hide');
-    if (vt1 != null) $('#iptbox .vl1').removeClass('hide');
-    if (vt2 != null) $('#iptbox .vl2').removeClass('hide');
-    if (vt3 != null) $('#iptbox .vl3').removeClass('hide');
+
+    if (vt1 != null && vt1 != "") $('#iptbox .vl1').removeClass('hide');
+    if (vt2 != null && vt2 != "") $('#iptbox .vl2').removeClass('hide');
+    if (vt3 != null && vt3 != "") $('#iptbox .vl3').removeClass('hide');
+
     $('#iptbox #vt0').text(vt0);
     $('#iptbox #vt1').text(vt1);
     $('#iptbox #vt2').text(vt2);
     $('#iptbox #vt3').text(vt3);
+
     $('#iptbox #v0').val(v0);
     $('#iptbox #v1').val(v1);
     $('#iptbox #v2').val(v2);
     $('#iptbox #v3').val(v3);
+
+    if (vm0 != "") {
+        $('#iptbox #vm0').text(vm0);
+        $('#iptbox #vm0').removeClass('hide');
+    }
+    if (vm1 != "") {
+        $('#iptbox #vm1').text(vm1);
+        $('#iptbox #vm1').removeClass('hide');
+    }
+    if (vm2 != "") {
+        $('#iptbox #vm2').text(vm2);
+        $('#iptbox #vm2').removeClass('hide');
+    }
+    if (vm3 != "") {
+        $('#iptbox #vm3').text(vm3);
+        $('#iptbox #vm3').removeClass('hide');
+    }
+
     showIpt('iptbox');
     if (v0 == "") {
         $('#iptbox #v0').focus();
@@ -124,6 +203,11 @@ function output(code, v1=null, v2=null, v3=null, vspt=false, nospace=false) {
 
 function iptbarOut() {
     var code = $('#iptbox').data('code');
+    var vspt = $('#iptbox').data('vspt');
+    // var vspt = false;
+    // if ($('#iptbox').data('vspt') == "1") {
+    //     vspt = true;
+    // }
     var v0 = $('#iptbox #v0').val();
     var v1 = $('#iptbox #v1').val();
     var v2 = $('#iptbox #v2').val();
@@ -132,7 +216,9 @@ function iptbarOut() {
     if (v2=="") v2=null;
     if (v3=="") v3=null;
 
-    var jsontext = BBCode(code, v0, v1, v2, v3);
+    v0 = v0.replaceAll('{{{SELECTTEXT}}}', selText('main-text'));
+
+    var jsontext = BBCode(code, v0, v1, v2, v3, vspt);
     $('#main-text').focus();
     $('#main-text').insertAtCaret(jsontext.text, jsontext.selStart, jsontext.selEnd);
     showIpt();
@@ -145,12 +231,23 @@ function BBCode(code, text=null, v1=null, v2=null, v3=null, vspt=false) {
     }
 
     if (v1 != null || v2 != null || v3 != null) {
+        v1 = v1.toString();
         if (v3 != null) {
-
+            v2 = v2.toString();
+            v3 = v3.toString();
+            return {
+                "text": "[" + code + "=" + v1 + "," + vt + v2 + "," + vt + v3 + "]" + text + "[/" + code + "]",
+                "selStart": code.length + 5 + v1.length + vt.length*2 + v2.length + v3.length,
+                "selEnd": code.length + 5 + v1.length + vt.length*2 + v2.length + v3.length + text.length,
+            };
         } else if (v2 != null) {
-
+            v2 = v2.toString();
+            return {
+                "text": "[" + code + "=" + v1 + "," + vt + v2 + "]" + text + "[/" + code + "]",
+                "selStart": code.length + 4 + v1.length + vt.length + v2.length,
+                "selEnd": code.length + 4 + v1.length + vt.length + v2.length + text.length,
+            };
         } else {
-            v1 = v1.toString();
             return {
                 "text": "[" + code + "=" + v1 + "]" + text + "[/" + code + "]",
                 "selStart": code.length + 3 + v1.length,
